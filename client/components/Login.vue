b4cb8ac81aff5d14cdc09a1fca16d04855d3785a
Rupam Shil <rupamshil111@gmail.com>
2021-09-07 15:02:05 +0530

    sign up page ui done

---
+++ client/components/Login.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="login-main">
+    <h1 class="header">{{ header }}</h1>
+    <form @submit.prevent="onSubmit">
+      <input type="text" required placeholder="Name" v-if="isSignUp" />
+      <input type="email" required placeholder="Email Address" />
+      <input type="password" required placeholder="Password" />
+      <button type="submit">
+        {{ header }}
+      </button>
+    </form>
+    <div class="login-signup">
+      <p v-if="!isSignUp">
+        Don't Have An Account?
+        <button type="button" @click="loginSignUpToggle">Sign Up</button>
+      </p>
+      <p v-else>
+        Already Have An Acocunt?
+        <button type="button" @click="loginSignUpToggle">Log In</button>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      header: 'Log In',
+      isSignUp: false,
+    }
+  },
+  methods: {
+    loginSignUpToggle() {
+      this.isSignUp = !this.isSignUp
+      if (this.isSignUp) {
+        this.header = 'Sign Up'
+      } else {
+        this.header = 'Log In'
+      }
+    },
+    onSubmit() {},
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.login-main {
+  padding: 2rem;
+  color: var(--white-color);
+  width: 100%;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    input {
+      outline: none;
+      border: none;
+      background: transparent;
+      border-bottom: 2px solid var(--primary-color-dark);
+      height: 40px;
+      color: var(--white-color);
+      &::placeholder {
+        color: var(--grey-text-color-dark);
+      }
+    }
+    button {
+      width: 30%;
+      align-self: center;
+      outline: none;
+      border: none;
+      padding: 0.4rem;
+      border-radius: 100vmax;
+      background: var(--button-color);
+      color: var(--white-color);
+      transition: 0.2s ease-in-out;
+      &:hover {
+        letter-spacing: 2px;
+      }
+    }
+  }
+  .login-signup {
+    button {
+      outline: none;
+      border: none;
+      background: transparent;
+      color: var(--primary-color);
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+}
+</style>
---
+++ client/pages/auth.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="main">
+    <div class="left">
+      <img src="~/assets/login.png" alt="" />
+    </div>
+    <div class="right">
+      <Login />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.main {
+  max-width: 1400px;
+  margin: 1rem auto;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .left {
+    flex: 1;
+    img {
+      width: 100%;
+    }
+  }
+  .right {
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+@media only screen and (max-width: 768px) {
+  .main {
+    flex-direction: column;
+  }
+}
+</style>
