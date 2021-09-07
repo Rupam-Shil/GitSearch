24002178e2bc2c99d8edddafb363286354c6f263
Rupam Shil <rupamshil111@gmail.com>
2021-09-07 12:15:52 +0530

    init

---
+++ client/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="">Hi<i class="uil uil-0-plus"></i></div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ server/server.js
@@ -0,0 +1,28 @@
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const defaultRoute = require('./routes/default');
+
+mongoose.connect(process.env.DB_MONGO);
+mongoose.connection
+	.once('open', () => {
+		console.log('DB connected');
+	})
+	.on('error', (err) => {
+		console.log(err);
+	});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/', defaultRoute);
+
+app.listen(3000, () => {
+	console.log('Running on port 3000');
+});
